74781618c0d300bb32b01bf31d4a0f9d27b39f99
unknown <watsapol.ping@gmail.com>
2024-09-30 14:01:06 +0700

    implement pricing

---
+++ components/Plans.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Check from "@/assets/check.svg";
+import { twMerge } from "tailwind-merge";
+import { TPlaning } from "@/interface/PlaningType";
+
+const Plans = ({
+  title,
+  popular,
+  price,
+  buttonText,
+  inverse,
+  features,
+}: TPlaning) => {
+  return (
+    <div className="max-w-[320px]">
+      <div className={`${inverse ? 'bg-black text-white' : 'bg-white'} boder-[#F1F1F1] px-10 py-10 rounded-3xl w-full shadow-[0_7px_14px_#EAEAEA]`}>
+        <div className="text-lg leading-7 tracking-tighter">
+          <div className="flex justify-between items-center">
+            <p className={twMerge("text-black/50 font-bold",inverse && 'text-white/60' )}>{title}</p>
+            {popular && (
+              <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                <p className="bg-[linear-gradient(to_right,#DD7DFF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DFF)] bg-clip-text text-transparent font-medium text-sm">
+                  Most Popular
+                </p>
+              </div>
+            )}
+          </div>
+
+          <h2 className="font-bold text-4xl mt-[30px]">
+            ${price}
+            <span className={twMerge("text-black/50 text-base", inverse && 'text-white/60')}> /month</span>
+          </h2>
+          <button className={twMerge("btn btn-primary w-full flex justify-center mt-[30px] font-medium text-base", inverse && 'bg-white text-black')}>
+            {buttonText}
+          </button>
+        </div>
+        <div className="mt-[34px] text-sm leading-5">
+          {features.map((feature, index) => (
+            <div key={index} className="flex gap-6 items-end mt-[22px]">
+              <Check className="w-6 h-6" />
+              <p>{feature}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
---
+++ sections/Pricing.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Plans from "@/components/Plans";
+import { pricingTicker } from "@/constants/pricingTickers";
+
+const Pricing = () => {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="text-center">
+            <h1 className="bg-gradient-to-t from-[#001E7F] to-black bg-clip-text text-transparent md:text-[54px] md:leading-[62px] tracking-tighter font-bold text-[30px]">
+              Pricing
+            </h1>
+            <p className="text-[#010D3E] leading-[30px] tracking-tight text-[22px] mt-4">
+              Free forever. Upgrade for unlimited tasks, better security, and
+              exclusive features.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row lg:items-end gap-6 justify-center items-center mt-10">
+          {pricingTicker.map((plan, index) => (
+            <Plans 
+              key={index}
+              title={plan.title}
+              price={plan.price}
+              buttonText={plan.buttonText}
+              popular={plan.popular}
+              inverse={plan.inverse}
+              features={plan.features}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Pricing;
