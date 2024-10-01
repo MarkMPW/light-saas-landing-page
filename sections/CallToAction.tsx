b67d98ed13001a971d243500d9e44ea1c4ab4dba
unknown <watsapol.ping@gmail.com>
2024-10-01 15:34:48 +0700

    implement animation in call to action part

--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+'use client'
+
+import React, { useRef } from "react";
 import Image from "next/image";
 
 import ArrowRight from "@/assets/arrow-right.svg";
 import star from "@/assets/star.png";
 import spring from "@/assets/spring.png";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const CallToAction = () => {
+  const ref = useRef(null)
+  const { scrollYProgress  } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"]
+  })
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150])
+
   return (
-    <section className="py-32 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] overflow-x-hidden">
+    <section ref={ref} className="py-32 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] overflow-hidden">
       <div className="container">
         <div className="text-center max-w-[540px] mx-auto relative">
           <h1 className="title">Sign up for free today</h1>
@@ -15,19 +26,25 @@ const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-            src={star}
+          <motion.img
+            src={star.src}
             alt="star"
             width={360}
             height={360}
-            className="absolute -top-[130px] -left-[350px] hidden md:flex"
+            className="absolute -top-[120px] -left-[350px] hidden md:flex"
+            style={{
+              translateY
+            }}
           />
-          <Image
-            src={spring}
+          <motion.img
+            src={spring.src}
             alt="spring"
             width={363}
             height={363}
             className="absolute -top-[19px] -right-[331px] hidden md:flex"
+            style={{
+              translateY
+            }}
           />
         </div>
         <div className="flex mt-12 justify-center">
